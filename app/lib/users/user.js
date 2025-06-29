2a1dc5283952fb6f2694f21984934877d7c0e094
sumaiya salim <sumaiyasalim062@gmail.com>
2025-06-29 21:43:10 +0600

    change

--- app/lib/users/user.js
+++ app/lib/users/user.js
@@ -12,32 +12,25 @@ export const createOrUpdateUser = async (
 ) => {
   try {
     await connect();
-
-    // Construct the $set data without null values
-    const updateData = {
-      firstName: first_name,
-      lastName: last_name,
-      profilePicture: image_url,
-      email: email_addresses?.[0]?.email_address,
-    };
-
-    // Only include username if it's not null/undefined/empty
-    if (typeof username === 'string' && username.trim() !== '') {
-      updateData.username = username;
-    }
-
     const user = await User.findOneAndUpdate(
       { clerkId: id },
-      { $set: updateData },
+      {
+        $set: {
+          firstName: first_name,
+          lastName: last_name,
+          profilePicture: image_url,
+          email: email_addresses[0].email_address,
+          username,
+        },
+      },
       { new: true, upsert: true }
     );
-
     return user;
   } catch (error) {
     console.log('Error creating or updating user:', error);
-    throw error;
   }
 };
+
 
 export const deleteUser = async (id) => {
   try {
