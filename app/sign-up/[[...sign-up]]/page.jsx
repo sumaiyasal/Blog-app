c1cd02789554fd18040d61d8094b3a92821fff29
sumaiya salim <sumaiyasalim062@gmail.com>
2025-06-29 23:53:51 +0600

    change

--- app/sign-up/[[...sign-up]]/page.jsx
+++ app/sign-up/[[...sign-up]]/page.jsx
@@ -1,6 +1,8 @@
 import { SignUp } from '@clerk/nextjs';
+import { headers } from 'next/headers';
 
-export default function SignUpPage() {
+export default async function SignUpPage() {
+  const h = await headers();
   return (
     <div className='flex items-center justify-center p-3'>
       <SignUp />
