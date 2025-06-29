64d0b59bc56545b5c8a4e5cffbc2798ceca535a7
sumaiya salim <sumaiyasalim062@gmail.com>
2025-06-30 00:10:22 +0600

    changee

--- app/api/webhooks/route.js
+++ app/api/webhooks/route.js
@@ -5,7 +5,7 @@ import { createOrUpdateUser, deleteUser } from '@/app/lib/users/user';
 
 export async function POST(req) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SIGNING_SECRET;
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
