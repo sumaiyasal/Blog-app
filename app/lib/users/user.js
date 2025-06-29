399c1ea6f742d25aec2646b20e4edd2b26279c0d
sumaiya salim <sumaiyasalim062@gmail.com>
2025-06-29 21:17:38 +0600

    update userid

--- app/lib/users/user.js
+++ app/lib/users/user.js
@@ -12,22 +12,30 @@ export const createOrUpdateUser = async (
 ) => {
   try {
     await connect();
+
+    // Dynamically build the update object
+    const updateData = {
+      firstName: first_name,
+      lastName: last_name,
+      profilePicture: image_url,
+      email: email_addresses[0]?.email_address,
+    };
+
+    // Only add username if it's a non-null string
+    if (username) {
+      updateData.username = username;
+    }
+
     const user = await User.findOneAndUpdate(
       { clerkId: id },
-      {
-        $set: {
-          firstName: first_name,
-          lastName: last_name,
-          profilePicture: image_url,
-          email: email_addresses[0].email_address,
-          username,
-        },
-      },
+      { $set: updateData },
       { new: true, upsert: true }
     );
+
     return user;
   } catch (error) {
     console.log('Error creating or updating user:', error);
+    throw error; // ⬅️ You might also want to rethrow for error handling upstream
   }
 };
 
