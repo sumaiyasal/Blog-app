146c1c8396219a91e67b9056bf1047361b3f9757
sumaiya salim <sumaiyasalim062@gmail.com>
2025-07-01 02:25:04 +0600

    add

---
+++ app/not-found.jsx
@@ -0,0 +1,12 @@
+// app/not-found.jsx
+
+export default function NotFound() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+        <p className="mt-2 text-gray-500">Sorry, the page you’re looking for doesn’t exist.</p>
+      </div>
+    </div>
+  );
+}
