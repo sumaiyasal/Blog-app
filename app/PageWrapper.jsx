1a4ddc23fac763e6a5e87c9e0b74f762e12f0f7c
sumaiya salim <sumaiyasalim062@gmail.com>
2025-07-01 03:00:37 +0600

    add

--- app/PageWrapper.jsx
+++ app/PageWrapper.jsx
@@ -1,11 +1,13 @@
 import React, { Suspense } from 'react';
 import SearchPage from './search/page';
+import Dashboard from './dashboard/page';
 
 
 export default function PageWrapper() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <SearchPage />
+      <Dashboard></Dashboard>
     </Suspense>
   );
 }
