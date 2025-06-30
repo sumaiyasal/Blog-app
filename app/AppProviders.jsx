50f01efacae13dd48c0d9769b791fbc8251fb62b
sumaiya salim <sumaiyasalim062@gmail.com>
2025-07-01 02:39:32 +0600

    add

--- app/AppProviders.jsx
+++ app/AppProviders.jsx
@@ -5,13 +5,16 @@ import { Provider } from 'react-redux';
 import { store } from './lib/redux/store';
 import Navbar from './Component/Navbar';
 import Footer from './Component/Footer';
+import { Suspense } from 'react';
 
 export default function AppProviders({ children }) {
   return (
     <ClerkProvider>
       <Provider store={store}>
         <Navbar />
-        {children}
+         <Suspense fallback={<div>Loading...</div>}>
+            {children}
+          </Suspense>
         <Footer />
       </Provider>
     </ClerkProvider>
